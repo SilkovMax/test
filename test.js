334688713ea9d92e8b5dc12640f4fa6484a9b25e
Maksim Silkov <silkov34@yandex.ru>
2019-05-26 08:34:04 +0300

    Добавил несколько переменных

--- test.js
+++ test.js
@@ -1,7 +1,19 @@
 ﻿'use strict'
 
 var a = 5; // Марк: добавил новую переменную
+var hello = "Привет друг"
+var userName = prompt(hello + ", как тебя зовут?");
+var ageUser = prompt(hello + ", сколько тебе лет?");
+if (isNaN(ageUser)) {
+  alert( "Строка преобразовалась в NaN. Не число" );
+  prompt(hello + ", сколько тебе лет?");
+} else {
+  alert( "Число" );
+}
+alert("Тебя зовут " + userName + " и тебе " + ageUser + " лет")
+alert(a+ageUser)
+alert(hello+a)
 
-console.log("Привет!");
+/*console.log("Привет!");
 alert("Hello world!")
-document.write("SilkovMaksim")
+document.write("SilkovMaksim")*/
