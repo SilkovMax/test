71f4a2bebd9f956faad8d93c1391602c3181a637
Maksim Silkov <silkov34@yandex.ru>
2019-06-26 19:41:51 +0300

    Попытка

--- test.js
+++ test.js
@@ -11,14 +11,24 @@ function doGetUserData() {
   Data = JSON.stringify(MaksimUser);
   console.log(Data);
 }
+let email = document.getElementById("emailUser").value;
+let phone = document.getElementById("phoneNumber").value;
 
-let x = new XMLHttpRequest();
+function validateEmail(email) {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+ 
+console.log( validateEmail(document.getElementById("emailUser").value) ); // false
+document.getElementById("button").addEventListener("click", validateEmail);
+
+/*let x = new XMLHttpRequest();
 x.onreadystatechange = function (){
   if(this.readyState == 4 && this.status == 200) {
     console.log(this.responseText);
   }
 };
-x.open("POST", "http://localhost:8080/send.php", true);
+x.open("POST", "https://wixihafi.myhostpoint.ch/send.php", true);
 x.send(Data);
 
-document.getElementById("button").addEventListener("click", doGetUserData);
+document.getElementById("button").addEventListener("click", doGetUserData);*/
